f3ff6699cc166cf6457d0206bcb21e12b031f7bf
YuzhuoZhang <yuzhuo.zhang@uconn.edu>
2024-02-13 14:26:31 -0500

    The MVCUnitTests.kt is in the quiz_unit_test.app.unitTest.

---
+++ app/src/test/MVCUnitTests.kt
@@ -0,0 +1,28 @@
+import org.junit.Assert.*
+import org.junit.Test
+class MVCUnitTests {
+    @Test
+    fun testNextQuestion() {
+        val controller = QuestionController()
+        val Question = controller.getCurrentQuestion()
+        controller.nextQuestion()
+        val nextQuestion = controller.getCurrentQuestion()
+        assertNotEquals(Question, nextQuestion, "The next question should be different from the initial one.")
+    }
+    @Test
+    fun testDisplaysQuestionCorrectly() {
+        val model = QuestionModel()
+        val question = model.getCurrentQuestion()
+        val view = QuestionView()
+        view.displayQuestion(question)
+        assertEquals(question.text, view.displayedText, "The view should display the text of the current question.")
+    }
+    fun testModelUpdatesCorrectly() {
+        val model = QuestionModel()
+        val initialIndex = model.getCurrentQuestionIndex()
+        model.nextQuestion()
+        val newIndex = model.getCurrentQuestionIndex()
+        assertTrue(newIndex > initialIndex, "The model should update to the next question index.")
+    }
+
+}
